2f0832091349a89ee90db4cf9f53b0002cec8c62
Naiade <nai.caparelli@gmail.com>
2025-08-06 15:09:39 -0300

    lp

--- src/components/core/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as OddsButton } from './OddsButton';
-export { default as MarketGroup } from './MarketGroup';
-export { default as BetSlip } from './BetSlip'; 
--- src/components/layouts/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as BiColumnLayout } from './BiColumnLayout';
-export { default as TriColumnLayout } from './TriColumnLayout';
-export { default as GridLayout } from './GridLayout';
-export { default as AccordionLayout } from './AccordionLayout';
-export { default as LadderLayout } from './LadderLayout';
-export { default as ListLayout } from './ListLayout';
-export { default as CarouselLayout } from './CarouselLayout';
-export { default as TabsLayout } from './TabsLayout'; 
